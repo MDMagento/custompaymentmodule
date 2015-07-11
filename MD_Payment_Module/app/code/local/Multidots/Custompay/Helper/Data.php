9ad387d557ee0541b482a4e7f8b9edfac05f737a
mdmilan <milan@multidots.in>
2015-07-11 17:38:06 +0530

    Multidots first magento module to have a custom simple payment method in Magento.

---
+++ MD_Payment_Module/app/code/local/Multidots/Custompay/Helper/Data.php
@@ -0,0 +1,6 @@
+<?php
+
+class Multidots_Custompay_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+}
---
+++ MD_Payment_Module/app/code/local/Multidots/Custompay/Model/Custommodel.php
@@ -0,0 +1,6 @@
+<?php
+class Multidots_Custompay_Model_Custommodel extends Mage_Payment_Model_Method_Abstract
+{
+	protected $_code = 'custompay';
+}
+?>
